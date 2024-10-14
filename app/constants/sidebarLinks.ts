521aca51c2cb5b6ed17f0e166562cee8e43f298e
RemusDB <remusd.buhaianu@gmail.com>
2024-10-14 14:28:24 +0300

    feat(constants): add sidebar links constants
    Created a constants file for the sidebar links to easily manage the sidebar navigation. Individual SVG component files are required for the 'icon' prop

---
+++ app/constants/sidebarLinks.ts
@@ -0,0 +1,37 @@
+export const SIDEBAR_LINKS = [
+  {
+    label: "Home",
+    icon: "",
+    path: "/",
+  },
+  {
+    label: "Posts",
+    icon: "",
+    path: "/content",
+  },
+  {
+    label: "Comments",
+    icon: "",
+    path: "/comment",
+  },
+  {
+    label: "Analytics",
+    icon: "",
+    path: "/analytics",
+  },
+  {
+    label: "Inspirations",
+    icon: "",
+    path: "/inspiration",
+  },
+  {
+    label: "Creator Academy",
+    icon: "",
+    path: "https://www.tiktok.com/creator-academy/",
+  },
+  {
+    label: "Feedback",
+    icon: "",
+    path: "/help/contact-us",
+  },
+];
