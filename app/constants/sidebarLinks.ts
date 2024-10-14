32340057cb18ee679cc377456408e5a8307cd1ce
RemusDB <remusd.buhaianu@gmail.com>
2024-10-14 16:00:46 +0300

    fix(sidebar): fix the relative pathing for sidebar link items

--- app/constants/sidebarLinks.ts
+++ app/constants/sidebarLinks.ts
@@ -7,31 +7,33 @@ import HomeIcon from "../icons/HomeIcon";
 import InspirationsIcon from "../icons/InspirationsIcon";
 import PostsIcon from "../icons/PostsIcon";
 
+const BASE_PATH = "/tiktokstudio";
+
 export const SIDEBAR_LINKS = [
   {
     label: "Home",
     icon: HomeIcon,
-    path: "/",
+    path: `${BASE_PATH}`,
   },
   {
     label: "Posts",
     icon: PostsIcon,
-    path: "/content",
+    path: `${BASE_PATH}/content`,
   },
   {
     label: "Comments",
     icon: CommentsIcon,
-    path: "/comment",
+    path: `${BASE_PATH}/comment`,
   },
   {
     label: "Analytics",
     icon: AnalyticsIcon,
-    path: "/analytics",
+    path: `${BASE_PATH}/analytics`,
   },
   {
     label: "Inspirations",
     icon: InspirationsIcon,
-    path: "/inspiration",
+    path: `${BASE_PATH}/inspiration`,
   },
   {
     label: "Creator Academy",
@@ -41,6 +43,6 @@ export const SIDEBAR_LINKS = [
   {
     label: "Feedback",
     icon: FeedbackIcon,
-    path: "/help/contact-us",
+    path: `${BASE_PATH}/help/contact-us`,
   },
 ];
--- app/tiktokstudio/_components/SidebarItem.tsx
+++ app/tiktokstudio/_components/SidebarItem.tsx
@@ -17,7 +17,7 @@ export default function SidebarItem({
     <li className="list-none max-w-fit p-2 my-2">
       <Link className="flex items-center gap-2" href={path}>
         <Icon width={24} height={24} />
-        <span className="text-base text-black font-medium">{label}</span>
+        <span className="text-base text-black font-normal">{label}</span>
       </Link>
     </li>
   );
