1d868472c3bfd0c093f458060771748a860b8e27
RemusDB <remusd.buhaianu@gmail.com>
2024-11-10 17:54:23 +0200

    chore: add query client and provider

--- app/tiktokstudio/layout.tsx
+++ app/tiktokstudio/layout.tsx
@@ -1,15 +1,22 @@
+"use client";
+
 // Components
 import Navbar from "./_components/Navbar";
 import Sidebar from "./_components/Sidebar";
 
+// React-Query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Navbar />
       <section className="flex pt-[62px]">
         <Sidebar />
         <main className="w-[calc(100%-240px)] ml-auto">{children}</main>
       </section>
-    </>
+    </QueryClientProvider>
   );
 }
